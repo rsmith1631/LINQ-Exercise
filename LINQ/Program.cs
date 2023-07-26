cf12be3d756deb69174af0dfcf211e3fed21cc33
rsmith1631 <rsmith1631@gmail.com>
2023-07-26 02:16:35 -0700

    Finished Linq project

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "Pacman", "Super Mario Brothers", "Halo", "Karate Man" };
+            var gameOrder = videoGames.OrderBy(games => games.Length);
+            
+            foreach (var game in gameOrder)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
